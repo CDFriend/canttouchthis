b6a4d51c0fcd23ebf4d43cfcc5f931e58316e315
Charlie Friend <charles.d.friend@gmail.com>
2017-10-29 19:42:06 -0700

    Create initial ugly chat GUI.

--- src/main/java/canttouchthis/ui/ConversationView.java
+++ src/main/java/canttouchthis/ui/ConversationView.java
@@ -1,15 +1,74 @@
 package canttouchthis.ui;
 
-import java.awt.Dimension;
+import java.awt.*;
 import javax.swing.*;
+import javax.swing.text.html.HTMLEditorKit;
 
+/**
+ * Window for sending and recieving chat messages (client or server side).
+ */
 public class ConversationView extends JFrame {
+
+    private final int FRAME_WIDTH = 300;
+    private final int FRAME_HEIGHT = 400;
+    private final int COMP_INSETS = 4;
 
     public ConversationView() {
         super("canttouchthis");
 
-        setPreferredSize(new Dimension(640, 480));
+        setResizable(false);
+        setPreferredSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        // Set grid bag layout and create contraints object
+        Container pane = getContentPane();
+        pane.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        // set external padding
+        c.insets = new Insets(COMP_INSETS, COMP_INSETS, COMP_INSETS, COMP_INSETS);
+
+        // chat window
+        JTextPane chatPane = new JTextPane();
+        JScrollPane chatScroll = new JScrollPane(chatPane);
+
+        c.gridx = 0;
+        c.gridy = 0;
+        c.weightx = 1.0;
+        c.weighty = 0.95;
+        c.gridwidth = 2;
+        c.fill = GridBagConstraints.BOTH;
+        chatPane.setEditable(false);
+        pane.add(chatScroll, c);
+
+        c.gridwidth = 1;
+
+        // add styled test string
+        chatPane.setEditorKit(new HTMLEditorKit());
+        chatPane.setText("<b>Whoa!!!</b>");
+
+        // send field and button
+        JTextField msgField = new JTextField(20);
+        c.gridx = 0;
+        c.gridy = 1;
+        c.weightx = 0.9;
+        c.weighty = 0.05;
+        c.ipadx = 2;
+        c.ipady = 20;
+        c.anchor = GridBagConstraints.CENTER;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        pane.add(msgField, c);
+
+        JButton sendButton = new JButton("Send");
+        c.gridx = 1;
+        c.gridy = 1;
+        c.weightx = 0.1;
+        c.weighty = 0.05;
+        c.ipadx = 2;
+        c.ipady = 20;
+        c.anchor = GridBagConstraints.CENTER;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        pane.add(sendButton, c);
 
         pack();
     }
