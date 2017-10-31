367f1a313dd3564a803d25f23870715b615c63f8
Charlie Friend <charles.d.friend@gmail.com>
2017-10-31 13:25:59 -0700

    Add function for checking message digests and add documentation.

--- src/main/java/canttouchthis/common/IntegrityChecking.java
+++ src/main/java/canttouchthis/common/IntegrityChecking.java
@@ -7,7 +7,13 @@ public class IntegrityChecking {
 
     private static final String A_DIG = "SHA-256";
 
-    private byte[] generateMessageDigest(String msg) {
+    /**
+     * Generates a SHA-256 digest hash for a given message.
+     *
+     * @param msg Message to create digest for.
+     * @return Byte sequence representing the hash.
+     */
+    public byte[] generateMessageDigest(String msg) {
 
         MessageDigest algo;
         try {
@@ -19,6 +25,25 @@ public class IntegrityChecking {
         }
 
         return algo.digest(msg.getBytes());
+
+    }
+
+    /**
+     * Checks whether or not a string message and its provided digest match.
+     * If this function returns false, the message was likely modified in
+     * transit.
+     *
+     * @param msg Message text.
+     * @param providedDigest SHA-256 digest provided along with the message.
+     * @return Whether or not the hashed text and its provided digest match.
+     */
+    public boolean checkDigest(String msg, byte[] providedDigest) {
+
+        // Generate a digest for the String message. This should be the same
+        // as the provided digest if the message has not been modified.
+        byte[] msgDigest = generateMessageDigest(msg);
+
+        return MessageDigest.isEqual(msgDigest, providedDigest);
 
     }
 
