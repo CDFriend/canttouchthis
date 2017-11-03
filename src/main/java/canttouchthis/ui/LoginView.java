524f6e9b6e1d846b2ffe30449a82841a1b06e05c
Charlie Friend <charles.d.friend@gmail.com>
2017-11-03 11:48:53 -0700

    Add login button to LoginView.

--- src/main/java/canttouchthis/ui/LoginView.java
+++ src/main/java/canttouchthis/ui/LoginView.java
@@ -23,25 +23,31 @@ public class LoginView extends JFrame {
 
         GridBagConstraints c = new GridBagConstraints();
 
+        // Username label and field
         c.gridx = 0;
         c.gridy = 0;
+        c.ipadx = 2;
+        c.ipady = 2;
         c.weightx = 0.15;
-        c.anchor = GridBagConstraints.EAST;
+        c.anchor = GridBagConstraints.WEST;
         c.fill = GridBagConstraints.NONE;
         JLabel uNameLabel = new JLabel("Username: ");
         pane.add(uNameLabel, c);
 
         c.gridx = 1;
         c.gridy = 0;
+        c.ipadx = 2;
+        c.ipady = 2;
         c.weightx = 0.85;
         c.fill = GridBagConstraints.HORIZONTAL;
         JTextField uNameField = new JTextField();
         pane.add(uNameField, c);
 
+        // Password label + field
         c.gridx = 0;
         c.gridy = 1;
         c.weightx = 0.15;
-        c.anchor = GridBagConstraints.EAST;
+        c.anchor = GridBagConstraints.WEST;
         c.fill = GridBagConstraints.NONE;
         JLabel passwordLabel = new JLabel("Password: ");
         pane.add(passwordLabel, c);
@@ -52,6 +58,13 @@ public class LoginView extends JFrame {
         c.fill = GridBagConstraints.HORIZONTAL;
         JPasswordField passwordField = new JPasswordField();
         pane.add(passwordField, c);
+
+        // Login button
+        c.gridx = 0;
+        c.gridy = 2;
+        c.gridwidth = 2;
+        JButton loginButton = new JButton("Login");
+        pane.add(loginButton, c);
 
         pack();
     }
