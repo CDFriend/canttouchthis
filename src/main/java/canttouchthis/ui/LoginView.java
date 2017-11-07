09316190f5cdcf2b7521eeba62106932a47211fd
Charlie Friend <charles.d.friend@gmail.com>
2017-11-07 13:49:11 -0800

    Add the man himself.

--- src/main/java/canttouchthis/ui/LoginView.java
+++ src/main/java/canttouchthis/ui/LoginView.java
@@ -9,11 +9,16 @@ public class LoginView extends JFrame {
     protected JPasswordField passwordField;
     protected JButton loginButton;
 
+    private ImageIcon _mchammer;
     private String _headerMessage;
 
     public LoginView(String headerMessage) {
         super("canttouchthis v1.0");
         this._headerMessage = headerMessage;
+
+        // MC Hammer gif courtesy of ci.memecdn.com
+        _mchammer = new ImageIcon("./assets/canttouchthis_crop.gif");
+
         initComponents();
     }
 
@@ -26,9 +31,17 @@ public class LoginView extends JFrame {
 
         GridBagConstraints c = new GridBagConstraints();
 
-        // header text
         c.gridx = 0;
         c.gridy = 0;
+        c.ipadx = 5;
+        c.ipady = 5;
+        c.gridwidth = 2;
+        JLabel hammerLabel = new JLabel(_mchammer);
+        pane.add(hammerLabel, c);
+
+        // header text
+        c.gridx = 0;
+        c.gridy = 1;
         c.ipadx = 10;
         c.ipady = 6;
         c.gridwidth = 2;
@@ -38,7 +51,7 @@ public class LoginView extends JFrame {
 
         // Username label and field
         c.gridx = 0;
-        c.gridy = 1;
+        c.gridy = 2;
         c.ipadx = 10;
         c.ipady = 2;
         c.weightx = 0.15;
@@ -49,7 +62,7 @@ public class LoginView extends JFrame {
         pane.add(uNameLabel, c);
 
         c.gridx = 1;
-        c.gridy = 1;
+        c.gridy = 2;
         c.ipadx = 2;
         c.ipady = 2;
         c.weightx = 0.85;
@@ -59,7 +72,7 @@ public class LoginView extends JFrame {
 
         // Password label + field
         c.gridx = 0;
-        c.gridy = 2;
+        c.gridy = 3;
         c.weightx = 0.15;
         c.ipadx = 10;
         c.anchor = GridBagConstraints.WEST;
@@ -68,7 +81,7 @@ public class LoginView extends JFrame {
         pane.add(passwordLabel, c);
 
         c.gridx = 1;
-        c.gridy = 2;
+        c.gridy = 3;
         c.weightx = 0.85;
         c.fill = GridBagConstraints.HORIZONTAL;
         passwordField = new JPasswordField();
@@ -76,7 +89,7 @@ public class LoginView extends JFrame {
 
         // Login button
         c.gridx = 0;
-        c.gridy = 3;
+        c.gridy = 4;
         c.gridwidth = 2;
         loginButton = new JButton("Login");
         pane.add(loginButton, c);
