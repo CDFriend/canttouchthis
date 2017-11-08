7f808823f140bc7ebd0e6b453545d41eb32e7e5c
Charlie Friend <charles.d.friend@gmail.com>
2017-11-07 16:36:04 -0800

    Add secure nonce for every user in auth_db.

--- generate_auth_db.py
+++ generate_auth_db.py
@@ -7,6 +7,12 @@ In production, this file would likely contain more accounts and be stored on a r
 
 Server test credentials: admin/nimda
 Client test credentials: user/resu
+
+Database schema:
+    uname STRING NOT NULL: Unique user ID
+    pwdHash STRING NOT NULL: SHA-256 hash of user's password (base64)
+    type STRING NOT NULL: Scope of user (either CLIENT or SERVER)
+    nonce STRING NOT NULL: 256-bit random number (base64)
 """
 
 import base64
@@ -35,14 +41,17 @@ def main():
     # create table for user data
     cur.execute("""
         CREATE TABLE data_users 
-            (uname STRING NOT NULL, pwdHash STRING NOT NULL, type STRING NOT NULL);
+            (uname STRING NOT NULL, pwdHash STRING NOT NULL, type STRING NOT NULL, nonce STRING NOT NULL);
     """)
 
     for uname, pwd, type in TEST_CREDENTIALS:
         pwd_digest = base64.b64encode(hashlib.sha256(str.encode(pwd)).digest())
         print("Adding row %s, %s" % (uname, pwd_digest))
 
-        cur.execute("INSERT INTO data_users VALUES (?, ?, ?);", (uname, pwd_digest, type))
+        # generate 256-bit random nonce for each user
+        nonce = base64.b64encode(os.urandom(32))
+
+        cur.execute("INSERT INTO data_users VALUES (?, ?, ?, ?);", (uname, pwd_digest, type, nonce))
 
     con.commit()
     con.close()
