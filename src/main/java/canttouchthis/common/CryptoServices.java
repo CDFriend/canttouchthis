bcaa56eb0d8f008ccae15acd18549e20b2164487
AJ Po-Deziel <aj.podeziel@gmail.com>
2017-11-08 21:02:16 -0800

    Refactor CryptoServices and added methods to retrieve Cipher objects

--- src/main/java/canttouchthis/common/CryptoServices.java
+++ src/main/java/canttouchthis/common/CryptoServices.java
@@ -23,9 +23,33 @@ public class CryptoServices {
 
     public CryptoServices() {
         byte[] ivbytes = { 0x00, 0x1a, 0x0f, 0x10, 0x44, 0x5c, 0x2d, 0x7c,
-                            0x22, 0x4f, 0x3a, 0x7c, 0x6a, 0x22, 0x1f, 0x13 };
+                           0x22, 0x4f, 0x3a, 0x7c, 0x6a, 0x22, 0x1f, 0x13 };
         this.ivspec = new IvParameterSpec(ivbytes);
     }
+
+    /**
+     * Retrieve encryption cipher.
+     * 
+     * @param key Key passed to method to encrypt under.
+     * @throws Exception For all possible exceptions (i.e. invalid key, bad padding, etc.)
+     */
+    public Cipher getEncryptCipher(Key key) throws Exception {
+        Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        c.init(Cipher.ENCRYPT_MODE, key, ivspec);
+    }
+
+
+    /**
+     * Retrieve decryption cipher.
+     * 
+     * @param key Key passed to method to encrypt under.
+     * @throws Exception For all possible exceptions (i.e. invalid key, bad padding, etc.)
+     */
+    public Cipher getDecryptCipher(Key key) throws Exception {
+        Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        c.init(Cipher.DECRYPT_MODE, key, ivspec);
+    }
+
 
     /**
      * Encrypt plaintext with AES.
@@ -40,7 +64,7 @@ public class CryptoServices {
         byte[] ciphertext = new byte[c.getOutputSize(plaintext.length)];
         c.doFinal(plaintext, 0, plaintext.length, ciphertext);
 
-        return ciphertext;
+        return c;
     }
 
     /**
