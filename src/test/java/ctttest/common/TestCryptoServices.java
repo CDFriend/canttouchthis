e3703a480e707698ad60f41b84942ae7515878ba
Charlie Friend <charles.d.friend@gmail.com>
2017-11-09 18:27:21 -0800

    Comment out deprecated unit test.

--- src/test/java/ctttest/common/TestCryptoServices.java
+++ src/test/java/ctttest/common/TestCryptoServices.java
@@ -12,24 +12,24 @@ import junit.framework.*;
 
 public class TestCryptoServices extends TestCase {
 
-    public void testEncryptDecrypt() throws Exception {
-        String message = "Hi there! This is a test message.";
-
-        //Generate random key for symmetric crypto
-        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
-        keyGen.init(128);
-        Key key = keyGen.generateKey();
-
-        /*
-         * Pass message to CryptoServices, 
-         * and then return its respective ciphertext back to it as well.
-         */
-        CryptoServices cs = new CryptoServices();
-        byte[] ciphertext = cs.encryptSymmetric(message.getBytes(), key);
-        String newPlainText = cs.decryptSymmetric(ciphertext, key);
-
-        assertEquals(message, newPlainText);
-    }
+//    public void testEncryptDecrypt() throws Exception {
+//        String message = "Hi there! This is a test message.";
+//
+//        //Generate random key for symmetric crypto
+//        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+//        keyGen.init(128);
+//        Key key = keyGen.generateKey();
+//
+//        /*
+//         * Pass message to CryptoServices,
+//         * and then return its respective ciphertext back to it as well.
+//         */
+//        CryptoServices cs = new CryptoServices();
+//        byte[] ciphertext = cs.encryptSymmetric(message.getBytes(), key);
+//        String newPlainText = cs.decryptSymmetric(ciphertext, key);
+//
+//        assertEquals(message, newPlainText);
+//    }
 
     /**
      * Checks that a CipherOutputStream using our CryptoUtils ciphers actually encrypts an
